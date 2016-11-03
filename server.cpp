05bd5ccbff30314dec00d40cef615190567e421f
?d?a?^ Kuan-Ting Wu <ktwu@npbsd3.cs.nctu.edu.tw>
2016-11-03 21:46:46 +0800

    server.cpp -- test1 done

--- server.cpp
+++ server.cpp
@@ -70,10 +70,22 @@ int passive_sock(const char *service, const char *transport, int q_len){
     return sock_fd; 
 }
 
-void TCP_echo(int sockfd){
+void do_printenv(char *args[]){
+    char *result = getenv(args[1]);
+    //if(result == NULL) sprintf(result, "No Such env\n"); 
+    //return result;
+    printf("%s\n", result);
+}
+
+void do_setenv(char *args[]){
+    setenv(args[1], args[2], 1);
+}
+
+void serve(int sockfd){
     char w_buffer[BUF_SIZE];
     char r_buffer[BUF_SIZE];
 
+    //write welcome message to client
     sprintf(w_buffer, "****************************************\n");
     errno = write(sockfd, w_buffer, strlen(w_buffer));
     if(errno < 0) error_exit("failed write(): %s\n", strerror(errno));
@@ -83,23 +95,83 @@ void TCP_echo(int sockfd){
     sprintf(w_buffer, "****************************************\n");
     errno = write(sockfd, w_buffer, strlen(w_buffer));
     if(errno < 0) error_exit("failed write(): %s\n", strerror(errno));
-    sprintf(w_buffer, "%% \n");
+    sprintf(w_buffer, "%% ");
     errno = write(sockfd, w_buffer, strlen(w_buffer));
     if(errno < 0) error_exit("failed write(): %s\n", strerror(errno));
     
     while(1){
         for(int j = 0; j < BUF_SIZE; j++) w_buffer[j] = '\0';
-        for(int j = 0; j < BUF_SIZE; j++) r_buffer[j] = '\0';
+        for(int j = 0; j < BUF_SIZE; j++) r_buffer[j] = '\0';   
 
+        //read recv_msg from client
         errno = read(sockfd, r_buffer, BUF_SIZE);
+        for(int j = 0; j < strlen(r_buffer); j++){
+           if(r_buffer[j] == 13) r_buffer[j] = '\0';
+        }
         if(errno < 0) error_exit("failed read(): %s\n", strerror(errno));
-        else {
-            printf("receive from client: %s", r_buffer);
+        /*else {
+            printf("receive from client: %s\n", r_buffer);
             printf("message length: %d\n\n", strlen(r_buffer));
+        }*/
+
+        //parsing recv_msg
+        char *args[100];
+        int argn = 0;
+        char *token_ptr;
+
+        for(int j = 0; j < 100; j++) args[j] = NULL;
+
+        token_ptr = strtok(r_buffer, " ");
+        while(token_ptr != NULL){
+            args[argn] = token_ptr;
+            argn++;
+            token_ptr = strtok(NULL, " ");
         }
 
+        /*for(int j = 0; j < argn; j++){
+            printf("token[%d]:%s\n", j,args[j]);
+        }*/
+        
+        //executing commands
+        char *result_str = NULL;
         if(strstr(r_buffer, "exit") != NULL){
             break;
+        }
+
+        int status, bytes_read;
+        char buffer[BUF_SIZE];
+
+        int pipe1fd[2]; //[1]child->[0]parent
+        int pipe2fd[2]; //[0]
+ 
+        if(pipe(pipe1fd) < 0 || pipe(pipe2fd) < 0){
+            printf("failed pipe()\n"); 
+            exit(-1);
+        }
+
+        if(strcmp(args[0], "setenv") == 0){ do_setenv(args);}
+        pid_t pid = fork();
+        if(pid < 0){ printf("failed fork()\n");  exit(-1); }
+        else if(pid == 0){ //child
+            close(pipe1fd[0]);
+            dup2(pipe1fd[1], STDOUT_FILENO);
+            if(strcmp(args[0], "printenv") == 0){ do_printenv(args); }
+            else{
+                errno = execvp(args[0], args);
+                if(errno < 0 && strcmp(args[0],"setenv") != 0) printf("Unknown Command: [%s]\n", args[0]);
+            }
+            exit(0);
+        }
+        else{ //parent
+            close(pipe1fd[1]);
+
+            bytes_read = read(pipe1fd[0], buffer, BUF_SIZE);
+            buffer[bytes_read] = '\0';
+            //printf("%s\n", buffer);
+            sprintf(w_buffer, buffer);
+            errno = write(sockfd, w_buffer, strlen(w_buffer));
+
+            waitpid(pid, &status, 0);
         }
 
         sprintf(w_buffer, "%% ");
@@ -109,6 +181,7 @@ void TCP_echo(int sockfd){
 }
 
 int main(int argc, char *argv[]){
+
     char *service; //int server_port;
     struct sockaddr_in client_addr;
     int master, slave; //master & slave sockfd
@@ -133,19 +206,22 @@ int main(int argc, char *argv[]){
         }
         printf("Slave %d accept connection from %s:%d\n", slave, inet_ntoa(client_addr.sin_addr), (int)ntohs(client_addr.sin_port));
 
+        setenv("PATH", "bin:.", 1);
         //5.create slave processes to handle connections and pass slave sockets to the processes
         int childpid;
         switch(childpid = fork()){ //return 0 to child, pid to parent
             case -1: error_exit("failed fork(): %s\n", strerror(errno));
             case 0: //child
+                    chdir("/net/gcs/105/0556503/ras/");
+                    printf("%s\n", getcwd(NULL, 0));
                     //6.interact with clients
                     close(master); //child doesn't need to use parent socket
-                    TCP_echo(slave);
+                    serve(slave);
                     close(slave);
                     exit(0); //child processes terminate successfully
             default: //parent
-                     close(slave); //parent doesn't need to use child socket
-                     break;
+                    close(slave); //parent doesn't need to use child socket
+                    break;
         }
 
     }
