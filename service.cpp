ca3cd7d5987acb10b7d0576ffc94ebd9aa925d55
?d?a?^ Kuan-Ting Wu <ktwu@npbsd3.cs.nctu.edu.tw>
2016-11-21 20:20:19 +0800

    test3 done

--- service.cpp
+++ service.cpp
@@ -17,26 +17,31 @@
 #include<sys/resource.h>
 #include<stdarg.h>
 
+#include<vector>
+using namespace std;
+
 #define BUF_SIZE 30000
-#define CMD_SIZE 2000
+#define NUM_OF_CMD 30000
+#define NUM_OF_RCD 1000
 struct cmd{
     int type; //0:normal 1:|n 2:> xxx.txt 3:!n
     int argn;
     char *args[1800];
 };
 
-/*void do_printenv(char *args[]){
-    char *result = getenv(args[1]);
-    printf("%s\n", result);
-}
+struct record{
+    int readOnly_fd;
+    int writeOnly_fd;
+    int count_down;
+};
+vector<struct record> table;
 
-void do_setenv(char *args[]){
-    setenv(args[1], args[2], 1);
-}*/
 void send_welcome_msg(int sockfd, char *buffer);
 int parse_received_msg(char *r_buffer, struct cmd* cmds);
 int execute_cmds(int num_of_cmd, struct cmd* cmds, int sockfd);
-void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd);
+void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd, int stdout_fd, bool input_from_past);
+
+int read_previous_pipe();
 ////////////////////////////////////////////////////////////////////////////
 void serve(int sockfd){
     char w_buffer[BUF_SIZE], r_buffer[BUF_SIZE];
@@ -48,10 +53,6 @@ void serve(int sockfd){
     sprintf(w_buffer, "%% ");
     w_bytes = write(sockfd, w_buffer, strlen(w_buffer));
     while(1){
-        /*for(int j = 0; j < BUF_SIZE; j++){
-            w_buffer[j] = '\0';
-            r_buffer[j] = '\0';
-        }*/
 
         //Step2.read messages from client
         bzero(r_buffer, BUF_SIZE);
@@ -65,11 +66,11 @@ void serve(int sockfd){
         }
 
         //Step3.parse received messages
-        struct cmd* cmds = (struct cmd*)malloc(sizeof(struct cmd)*30000);
+        struct cmd* cmds = (struct cmd*)malloc(sizeof(struct cmd)*NUM_OF_CMD);
         int num_of_cmd;
         num_of_cmd = parse_received_msg(r_buffer, cmds);
 
-        printf("/////%d/////\n",num_of_cmd);
+        /*printf("/////%d/////\n",num_of_cmd);
         for(int j = 0; j < num_of_cmd; j++){
             printf("cmd[%d] ", j+1);
             printf("type%d: ", cmds[j].type);
@@ -77,7 +78,7 @@ void serve(int sockfd){
                 printf("%s ", cmds[j].args[k]);
             }
             printf("\n");
-        }
+        }*/
        
         int result_fd = execute_cmds(num_of_cmd, cmds, sockfd);
         bzero(w_buffer, BUF_SIZE);
@@ -88,13 +89,14 @@ void serve(int sockfd){
         free(cmds);
 
         //Step4.
-
         bzero(w_buffer, BUF_SIZE);
         sprintf(w_buffer, "%% ");
         w_bytes = write(sockfd, w_buffer, strlen(w_buffer));
         if(w_bytes < 0) printf("failed write()\n");
 
     }
+
+    table.clear();
 }
 
 void send_welcome_msg(int sockfd, char *buffer){
@@ -150,7 +152,7 @@ int parse_received_msg(char *r_buffer, struct cmd* cmds){
             cmds[j].argn++;    
         }
         //printf("\n");
-        cmds[j].args[cmds[j].argn] = 0;
+        cmds[j].args[cmds[j].argn] = NULL;
     }
     
     return num_of_cmd;
@@ -168,7 +170,8 @@ int execute_cmds(int num_of_cmd, struct cmd* cmds, int sockfd){
     int stdout_fd = dup(1);
     int stderr_fd = dup(2);
 
-    int input_fd = 0; //read args of current cmd from here
+    int input_fd = read_previous_pipe(); //read args of current cmd from here
+    bool input_from_previous_pipe = (input_fd == 0)?false:true;
     int output_fd = out_pipe[1]; //write the output of a command to this
     int next_input_fd = out_pipe[0]; //read next cmd's input from here
 
@@ -178,7 +181,8 @@ int execute_cmds(int num_of_cmd, struct cmd* cmds, int sockfd){
         dup2(input_fd, 0);
         dup2(output_fd, 1);
 
-        execute_single_cmd(cmds[i], i,input_fd, output_fd);
+        execute_single_cmd(cmds[i], i,input_fd, output_fd, stdout_fd, input_from_previous_pipe);
+        input_from_previous_pipe = false; //used when i==0
 
         if(i != 0) close(input_fd); //can't close stdin
         close(output_fd);
@@ -193,16 +197,23 @@ int execute_cmds(int num_of_cmd, struct cmd* cmds, int sockfd){
     dup2(stdin_fd, 0);
     dup2(stdout_fd, 1);
 
+    vector<struct record>::iterator it;
+    printf("talbe size:%d\n",table.size());
+    for(it = table.begin(); it != table.end(); it++){
+        printf("readOnly:%d writeOnly:%d count_down:%d\n", it->readOnly_fd, it->writeOnly_fd, it->count_down);
+    }
+    printf("/////////////\n");
+
     return input_fd;    
 }
-void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd){
+void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd, int stdout_fd, bool input_from_past){
     char buffer[BUF_SIZE];
     bzero(buffer,BUF_SIZE);
 
     if(command.type == 0){
         if(strcmp(command.args[0], "printenv") == 0){
             char* result = getenv(command.args[1]);
-            sprintf(buffer, "%s\n", result);
+            sprintf(buffer, "PATH:%s\n", result);
             write(output_fd, buffer, strlen(buffer));
         }
         else if(strcmp(command.args[0], "setenv") == 0){
@@ -210,23 +221,68 @@ void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd){
             write(output_fd, buffer, strlen(buffer));
         }
         else{
+            vector<struct record>::iterator it;
+            for(it = table.begin(); it != table.end(); it++){
+                if(it->count_down == 0){
+                    break;
+                }
+            }
+
             pid_t pid= fork();
             int status;
             if(pid < 0){ exit(1); }
             else if(pid == 0){ //child
+                if(input_from_past) close(it->writeOnly_fd);
                 execvp(command.args[0], command.args);
                 char error_msg[100];
-                sprintf(error_msg, "Unknown command:[%s]\n", command.args[0]);
+                sprintf(error_msg, "Unknown command:[%s].\n", command.args[0]);
                 write(output_fd, error_msg, strlen(error_msg));
                 exit(1);
             }
             else{ //parent
+                if(input_from_past) close(it->writeOnly_fd);
                 waitpid(pid, &status, 0);
+                if(input_from_past){
+                    close(it->readOnly_fd);
+                    table.erase(it);
+                }
             }
         }
     }
     else if(command.type == 1){ //|n
-
+        int count;
+        sscanf(command.args[0], "|%d", &count);
+        
+        bool create_new_pipe = true;
+        vector<struct record>::iterator it;
+        for(it = table.begin(); it != table.end(); it++){
+            if(it->count_down == count){
+                create_new_pipe = false;
+                break;
+            }
+        }
+        
+        int newpipe_fd[2];
+        if(create_new_pipe){
+            //create a new pipe;
+            pipe(newpipe_fd);
+            //record the new pipe information
+            struct record temp;
+            temp.count_down = count;
+            temp.readOnly_fd = newpipe_fd[0];
+            temp.writeOnly_fd = newpipe_fd[1];
+            table.push_back(temp);
+        }
+        
+        //read from input_fd and write to output_fd
+        read(input_fd, buffer, BUF_SIZE);
+        //write(output_fd, buffer, strlen(buffer));
+        if(create_new_pipe){
+            write(newpipe_fd[1], buffer, strlen(buffer));
+        }
+        else{
+            write(it->writeOnly_fd, buffer, strlen(buffer));
+        }
     }
     else if(command.type == 2){ //!n
 
@@ -236,4 +292,15 @@ void execute_single_cmd(struct cmd command, int i, int input_fd, int output_fd){
         read(input_fd, buffer, BUF_SIZE);
         write(fileno(fptr), buffer, strlen(buffer));
     }
+}
+
+int read_previous_pipe(){ //read input from previous cmd using pipe saved in table
+    vector<struct record>::iterator it;
+    for(it = table.begin(); it != table.end(); it++){
+            it->count_down--;
+            if(it->count_down == 0){
+                return it->readOnly_fd;
+            }
+        }
+    return 0;
 }
